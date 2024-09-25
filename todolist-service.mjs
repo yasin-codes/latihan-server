45e8d0728bc15ddc558cba5c35ff65dee2809dbf
yasinsuckz <ahmadyasin.abdr@gmail.com>
2024-09-25 20:35:24 +0700

    second commit

--- todolist-service.mjs
+++ todolist-service.mjs
@@ -1,23 +1,43 @@
-import { todo } from "node:test";
+// import { todo } from "node:test";
 
 export class TodolistService {
-  todolist = {
-    id: 1,
-    todo: "Belajar NodeJS",
-  };
+  todolist = [
+    {
+      id: 1,
+      todo: "Belajar NodeJS",
+    },
+    {
+      id: 2,
+      todo: "Belajar JavaScript",
+    },
+  ];
 
-  getTodolist(req, res) {
-    res.write(
-      JSON.stringify({
-        code: 200,
-        status: "OK",
-        data: this.todolist,
-      })
-    );
-    return this.todolist;
+  getJsonTodolist() {
+    JSON.stringify({
+      code: 200,
+      status: "OK",
+      data: this.todolists,
+    });
+    return JSON.stringify(this.todolists);
+    res.end();
   }
 
-  postTodolist(req, res) {}
+  getTodolist(req, res) {
+    res.write(this.getJsonTodolist);
+    return this.todolist;
+    res.end();
+  }
+
+  createTodolist(req, res) {
+    req.addListener("data", (chunk) => {
+      const body = JSON.parse(chunk.toString());
+      this.todolist.push({
+        id: this.todolist.length + 1,
+        todo: body.todo,
+      });
+      this.getTodolist(req, res);
+    });
+  }
 
   putTodolist(req, res) {}
 
